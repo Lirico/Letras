49a3e143cf9eed20b5e7c73b465c3b940a7b42d2
Lirico <dominguezmatiasadrian@gmail.com>
2020-11-08 00:51:39 -0300

    version 5

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,27 +2,27 @@ import React, { useState } from 'react';
 
 const Formulario = ({guardarBusquedaLetra}) => {
 
-    const [ busqueda, guardarBusqueda ] = useState({
+    const [busqueda, guardarBusqueda] = useState({
         artista: '',
         cancion: ''
-    })
+    });
     const [ error, guardarError] = useState(false);
 
     const { artista, cancion } = busqueda;
-    
+
     // función a cada input para leer su contenido
     const actualizarState = e => {
         guardarBusqueda({
             ...busqueda,
             [e.target.name] : e.target.value
-        });
+        })
     }
-    
+
     // consultar las apis
     const buscarInformacion = e => {
         e.preventDefault();
 
-        if(artista.trim() === '' || cancion.trim === '') {
+        if(artista.trim() === '' || cancion.trim() === ''){
             guardarError(true);
             return;
         }
@@ -34,10 +34,11 @@ const Formulario = ({guardarBusquedaLetra}) => {
 
     return ( 
         <div className="bg-info">
-            { error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p> : null }
+            { error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p> : null}
             <div className="container">
                 <div className="row">
-                    <form
+                    
+                    <form 
                         onSubmit={buscarInformacion}
                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
                     >
@@ -48,7 +49,7 @@ const Formulario = ({guardarBusquedaLetra}) => {
                                 <div className="col-md-6">
                                     <div className="form-group">
                                         <label>Artista</label>
-                                        <input 
+                                        <input
                                             type="text"
                                             className="form-control"
                                             name="artista"
@@ -57,18 +58,20 @@ const Formulario = ({guardarBusquedaLetra}) => {
                                             value={artista}
                                         />
                                     </div>
+                                    
                                 </div>
-
                                 <div className="col-md-6">
-                                    <label>Canción</label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        name="cancion"                                       
-                                        placeholder="Nombre Canción"
-                                        onChange={actualizarState}
-                                        value={cancion}
-                                    />
+                                    <div className="form-group">
+                                        <label>Canción</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="Nombre Canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
+                                        />
+                                    </div>
                                 </div>
                             </div>
 
@@ -77,11 +80,12 @@ const Formulario = ({guardarBusquedaLetra}) => {
                                 className="btn btn-primary float-right"
                             >Buscar</button>
                         </fieldset>
+
                     </form>
                 </div>
             </div>
         </div>
-    );
+     );
 }
  
 export default Formulario;
